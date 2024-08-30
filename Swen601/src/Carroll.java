8ca65d4e9d67f2bfe331a9a27e543fa064339e31
Pratibha-mscs <pratibha.mscs@gmail.com>
2024-08-30 16:45:19 -0400

    Add Lewis Carroll quote

---
+++ Swen601/src/Carroll.java
@@ -0,0 +1,10 @@
+public class Carroll {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\"Begin at the");
+        System.out.println("beginning,\", the King");
+        System.out.println("said, very gravely, \"and"); 
+        System.out.println("go on till you come to");
+        System.out.println("the end: then stop.\"");
+        System.out.println("\t -Lewis Carroll");
+    }
+}
