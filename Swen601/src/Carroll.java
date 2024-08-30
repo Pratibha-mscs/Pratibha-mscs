02723a559516c3d495781f9eafeb40cb687a8c74
Pratibha-mscs <pratibha.mscs@gmail.com>
2024-08-30 16:54:52 -0400

    Added comments

--- Swen601/src/Carroll.java
+++ Swen601/src/Carroll.java
@@ -1,10 +1,22 @@
+/*
+    Software Construction
+    Pratibha Giri 
+    pg6915@g.rit.edu
+    The purpose of the assignment is to leave Java and get familiar with git.
+*/
 public class Carroll {
+/*
+    The main method here is printing the Lewis Carroll's quote.
+*/
+    
     public static void main(String[] args) throws Exception {
         System.out.println("\"Begin at the");
         System.out.println("beginning,\", the King");
         System.out.println("said, very gravely, \"and"); 
         System.out.println("go on till you come to");
         System.out.println("the end: then stop.\"");
-        System.out.println("\t -Lewis Carroll");
+        System.out.println("-Lewis Carroll");
     }
-}
+} // End of the class Carroll  
+
+
